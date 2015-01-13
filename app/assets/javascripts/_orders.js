b55a1658d1625fbe7553a6616755b0ace9a5d15e
Tessa <tessakkelly@gmail.com>
2015-01-13 11:27:53 -0800

    fix cart button two displays; cart view and all items view:

--- app/assets/javascripts/_orders.js
+++ app/assets/javascripts/_orders.js
@@ -1,6 +1,10 @@
 app.controller('cartController', ['$scope', 'cartService', function($scope, cartService) {
   $scope.cartContents = cartService.getCart();
-  $scope.showList = cartService.showList;
+
+  $scope.showList = function() {
+    return cartService.showList();
+  }
+
   $scope.totalPrice = function() {
     var total = 0;
     $scope.cartContents.forEach(function(v,i,a) {
