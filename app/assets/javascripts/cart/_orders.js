5abe777031ef9a234061a167215d526d7a6a18bd
Tessa <tessakkelly@gmail.com>
2015-01-15 15:53:34 -0800

    order form add cartController, check orderName existence before posting order to db

--- app/assets/javascripts/cart/_orders.js
+++ app/assets/javascripts/cart/_orders.js
@@ -1,9 +1,6 @@
 app.controller('cartController', ['$scope', 'Orders', 'cartService', 'Show', function($scope, Orders, cartService, Show) {
   $scope.cartContents = cartService.getCart();
 
-  $scope.show = function() {
-    return Show.show();
-  }
   $scope.updateShow = Show.updateShow;
 
   $scope.totalPrice = function() {
@@ -21,15 +18,18 @@ app.controller('cartController', ['$scope', 'Orders', 'cartService', 'Show', fun
   }
 
   $scope.checkout = function(e) {
+    $scope.showForm = true;
     angular.element(".remove-from-order").addClass('hide');
     angular.element(".checkout").addClass('hide');
   }
 
   $scope.submitOrder = function() {
-    $scope.hideForm = true;
-    Orders.postOrder({person: $scope.orderName, cost: $scope.totalPrice()});
-    cartService.completeOrder(true);
-    angular.element(".form-horizontal").before("<div class='container'><h5>Your order has been submitted.<br> Please save a copy of this page for your records.</h5></div>");
+    if($scope.orderName) {
+      $scope.hideForm = true;
+      Orders.postOrder({person: $scope.orderName, cost: $scope.totalPrice()});
+      cartService.completeOrder(true);
+      angular.element(".form-horizontal").before("<div class='container'><h5>Your order has been submitted.<br> Please save a copy of this page for your records.</h5></div>");
+    }
   }
 
 }]);
