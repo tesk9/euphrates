09c870e45f1cc832b46c3efe274f83271a10e582
Tessa <tessakkelly@gmail.com>
2015-01-15 15:04:20 -0800

    Navbar formatting fix, add links to ul

--- app/assets/javascripts/shared/showView.js
+++ app/assets/javascripts/shared/showView.js
@@ -1,19 +1,11 @@
 app.factory('Show', ['$location', function($location) {
-  var show;
   return {
-    show: function() {
-      if(show === undefined) { 
-        show = true; 
-      }
-      return show;
-    },
     updateShow: function() {
       if($location.path() === '/') {
         $location.path('/cart');
       } else {
         $location.path('/');
       }
-      show = !show;
     } 
   }
 }])
